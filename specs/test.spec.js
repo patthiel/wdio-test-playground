199c1137247a7cb9f91c62f0c420a110086a9388
patthiel <pthiel@anthropologie.com>
2016-10-10 18:12:47 -0400

    added notes to spec

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,14 +1,16 @@
-describe('Wdio waitforExist Suite', () => {
+describe('WDIO waitforExist Suite', () => {
 
+	// This test will Fail in the WDIO Runner
     it('waitForExist Timeout - test will FAIL', () => {
         browser.url('http://www.apple.com');
         browser.waitForExist('something', 5000);	
     });
 
+    // This test will be executed and should fail, but the results are not outputted
     it('waitForExist Timeout - test SHOULD FAIL', () => {
 	    browser.waitForExist('.ac-gn-link ac-gn-link-watch', 5000);
     });
-
+    // This test will be executed and should pass, but the results are not outputted
     it('waitForExist Succeeds - Test should PASS', () => {
     	browser.waitForExist('.ac-gn-link', 5000);
     });
